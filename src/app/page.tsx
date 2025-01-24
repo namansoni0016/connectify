647a41136c2bbb495768cdd212acebedeed24a0d
Naman Soni <namantsoni@gmail.com>
2025-01-24 11:46:53 +0530

    Complete Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
+      <h1>Home Page Content</h1>
     </div>
   );
 }
