d467da449fb463c79f01ee790d3f51eaf0e00121
Naman Soni <namantsoni@gmail.com>
2025-02-06 09:49:14 +0530

    Suggested Users

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -1,6 +1,7 @@
 "use server";
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
     try {
@@ -51,4 +52,91 @@ export async function getDbUserId() {
     const user = await getUserByClerkId(clerkId);
     if(!user) throw new Error("User not found!");
     return user.id;
+}
+
+export async function getRandomUsers() {
+    try {
+        const userId = await getDbUserId();
+        // get 3 random users exclude ourselves & users that we already follow
+        const randomUsers = await prisma.user.findMany({
+            where: {
+                AND: [
+                    {NOT: {id: userId}},
+                    {
+                        NOT: {
+                            followers: {
+                                some: {
+                                    followerId: userId
+                                }
+                            }
+                        }
+                    },
+                ]
+            },
+            select: {
+                id: true,
+                name: true,
+                username: true,
+                image: true,
+                _count: {
+                    select: {
+                        followers: true,
+                    }
+                }
+            },
+            take: 3,
+        });
+        return randomUsers;
+    } catch (error) {
+        console.log("Error fetching random users: ", error);
+        return [];
+    }
+}
+
+export async function toggleFollow(targetUserId : string) {
+    try {
+        const userId = await getDbUserId();
+        if(userId === targetUserId) throw new Error("You cannot follow yourself!");
+        const exsistingFollow = await prisma.follows.findUnique({
+            where: {
+                followerId_followingId: {
+                    followerId: userId,
+                    followingId: targetUserId
+                }
+            }
+        })
+        if(exsistingFollow) {
+            //unfollow
+            await prisma.follows.delete({
+                where: {
+                    followerId_followingId: {
+                        followerId: userId,
+                        followingId: targetUserId
+                    }
+                }
+            })
+        } else {
+            //follow
+            await prisma.$transaction([
+                prisma.follows.create({
+                    data: {
+                        followerId: userId,
+                        followingId: targetUserId,
+                    }
+                }),
+                prisma.notification.create({
+                    data: {
+                        type: "FOLLOW",
+                        userId: targetUserId, //User being followed
+                        creatorId: userId
+                    }
+                })
+            ])
+        }
+        revalidatePath("/");
+        return { success: true }
+    } catch (error) {
+        console.log("Error in toggleFollow: ", error);
+        return { success: false, error: "Error toggling follow" }
+    }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server";
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 
 export default async function Home() {
   const user = await currentUser();
@@ -8,7 +9,9 @@ export default async function Home() {
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
       </div>
-      <div className="hidden lg:block lg:col-span-4 sticky top-20">Follow</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
